9f4220fc0aa98951e71ff9e64c649a144b35a6a0
Pavol Rajzak <pavol.rajzak@itera.no>
2016-07-12 17:26:22 +0200

    Last minor fixes

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -39,10 +39,10 @@ const common = {
       },
       {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
       {test: /\.css$/, loader: 'style-loader!css-loader'},
-      {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-      {test: /img\/\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+      {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /img\/\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   }, plugins: [
     new webpack.ProvidePlugin({
